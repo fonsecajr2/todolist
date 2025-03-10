35e17acfa83341de59fe5f7251c34cc888f8b4f6
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-03-10 12:32:18 +0530

    Antes do TPC

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -1,15 +1,48 @@
+import {ChevronRightIcon} from "lucide-react";
+import { TrashIcon } from "lucide-react";
+import clsx from "clsx";
+
 function Tasks (props) {
     console.log(props)
-    return(
+
+    function onTaskClick(taskId){
+        if (!props.tasks || !Array.isArray(props.tasks)) {
+            console.error("Erro: props.tasks não está definido ou não é um array.");
+            return;
+        }
+
+        const newTasks = props.tasks.map(task => {
+            if (task.id === taskId){
+                return {...task, isCompleted: !task.isCompleted}
+            }else{
+                return task
+            }
+        });
+
+        props.setTasks(newTasks);
+    }
+
+    return (
         <div>
             <br />
             <h1>Tasks</h1>
             <br />
             <ul className="space-y-4 p-6 bg-[#EBE5C2] rounded-md shadow">
                 {props.tasks.map((task) => (
-                    <li key={task.id} className="flex">
-                        <button className="bg-[#B9B28A] text-[#504B38] p-2 rounded-md w-full">{task.title}</button>
-                        <button className="bg-[#B9B28A] text-[#504B38] p-2 rounded-md">Ver Detalhes</button>
+                    <li key={task.id} className="flex gap-2">
+                        <button  onClick= {() => onTaskClick(task.id)}
+                            className={clsx(
+                                "text-left bg-[#B9B28A] text-[#504B38] p-2 rounded-md w-full",
+                                task.isCompleted && "line-through"
+                            )} >
+                                {task.title}
+                        </button>
+                        <button className="bg-[#B9B28A] text-[#504B38] p-2 rounded-md">
+                            <ChevronRightIcon />
+                        </button>
+                        <button className="bg-[#B9B28A] text-[#504B38] p-2 rounded-md">
+                            <TrashIcon />
+                        </button>
                     </li>
                 )) }
             </ul>
